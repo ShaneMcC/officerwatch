fecca7be2998a6b30c4d6c702279e1b851e910ec
Shane Mc Cormack <shanemcc@gmail.com>
2019-04-16 23:27:56 +0100

    Upstream has archived, so reformat a bit for self-maintainability.

--- doStuff.php
+++ doStuff.php
@@ -1,55 +1,51 @@
 <?php
 
-if (!file_exists(__DIR__ . '/config.php')) {
-	echo 'Please set up config.php'.PHP_EOL;
-	die();
-}
-
-require_once(__DIR__ . '/config.php');
-
-foreach ($officers as $officer) {
-	echo 'Checking '.$officer['name'].'...'.PHP_EOL;
-
-	$url = 'https://api.companieshouse.gov.uk/officers/'.$officer['officer_id'].'/appointments';
-	$curlRes = curl_init();
-	curl_setopt($curlRes, CURLOPT_URL, $url);
-	curl_setopt($curlRes, CURLOPT_USERPWD, API_KEY.':');
-	curl_setopt($curlRes, CURLOPT_HEADER, false);
-	curl_setopt($curlRes, CURLOPT_RETURNTRANSFER, true);
-	$result = curl_exec($curlRes);
-	curl_close ($curlRes);
-
-	$result = json_decode($result, TRUE);
-
-	$new = [];
-	foreach ($result['items'] as $company) {
-		$director = $company['appointed_to'];
-
-		//TODO: put this in sql lite or something that isnt files on disk
-		$path = CACHE.$director['company_number'].'_'.$officer['officer_id'].'.txt';
-		if (!file_exists($path)) {
-			$new[] = $director['company_name'];
-	                file_put_contents($path, NULL);
-		}
+	if (!file_exists(__DIR__ . '/config.php')) {
+		echo 'Please set up config.php', "\n";
+		die();
 	}
 
-	if (count($new) > 0) {
-		$message = $officer['name'].' has new appointments on companies house. View more details at https://beta.companieshouse.gov.uk/officers/'.$officer['officer_id'].'/appointments'.PHP_EOL.PHP_EOL;
-		$notify = '';
-		foreach ($officer['notify'] as $email) {
-			$notify .= $email.',';
+	require_once(__DIR__ . '/config.php');
+
+	foreach ($officers as $officer) {
+		echo 'Checking ', $officer['name'], '...', "\n";
+
+		$url = 'https://api.companieshouse.gov.uk/officers/' . $officer['officer_id'] . '/appointments';
+		$curlRes = curl_init();
+		curl_setopt($curlRes, CURLOPT_URL, $url);
+		curl_setopt($curlRes, CURLOPT_USERPWD, API_KEY . ':');
+		curl_setopt($curlRes, CURLOPT_HEADER, false);
+		curl_setopt($curlRes, CURLOPT_RETURNTRANSFER, true);
+		$result = curl_exec($curlRes);
+		curl_close($curlRes);
+
+		$result = json_decode($result, TRUE);
+
+		$new = [];
+		foreach ($result['items'] as $company) {
+			$director = $company['appointed_to'];
+
+			//TODO: put this in sql lite or something that isnt files on disk
+			$path = CACHE . $director['company_number'] . '_' . $officer['officer_id'] . '.txt';
+			if (!file_exists($path)) {
+				$new[] = $director['company_name'];
+				file_put_contents($path, NULL);
+			}
 		}
-		foreach ($new as $appointment) {
-			echo 'NEW APPOINTMENT - '.$appointment.PHP_EOL;
-			$message .= $appointment.PHP_EOL;
+
+		if (count($new) > 0) {
+			$message = $officer['name'] . ' has new appointments on companies house. View more details at https://beta.companieshouse.gov.uk/officers/' . $officer['officer_id'] . '/appointments' . "\n\n";
+			$notify = implode(',', $officer['notify']);
+			foreach ($new as $appointment) {
+				echo 'NEW APPOINTMENT - ', $appointment, "\n";
+				$message .= $appointment . "\n";
+			}
+			$subject = '[Officer Watch] ' . $officer['name'];
+			$to = MAIL_TO;
+			$headers = implode("\r\n", ['From: ' . MAIL_FROM, 'BCC: ' . $notify]);
+			mail($to, $subject, $message, $headers);
+		} else {
+			echo 'Nothing new', "\n";
 		}
-		$subject = '[Officer Watch] '.$officer['name'];
-		$to = MAIL_TO;
-		$headers = 'From: '.MAIL_FROM."\r\n".
-			'BCC: '.$notify;
-		mail($to, $subject, $message, $headers);
-	} else {
-		echo 'Nothing new'.PHP_EOL;
 	}
-}
 
